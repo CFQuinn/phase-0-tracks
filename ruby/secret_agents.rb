107365376fcaf49fbf188b0addfa96b07ad2eef9
CFQuinn <chrisquinones55@gmail.com>
2016-11-05 22:21:47 -0500

    added dycrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,18 +1,56 @@
 #asking for a word to be encrypted
-p "please provide a word."
-input= gets.chomp
+#p "please provide a word."
+#input= gets.chomp
 
 #method for encryption
-def word(str)
+def encrypt(str)
 	index = 0
-	while
+	# declare empty string we will add to
+	encrypted = ""
+	alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		# loop over the string's letters
 		while index < str.length
-		  str[index] = str[index].next!
-		  index += 1
+		letter = str[index]
+		  if letter == ''
+		  	encrypted << ''
+		 #conditioning "a" as step forward for "z"
+		  elsif letter == "z"
+		  	encrypted << "a"
+		  elsif letter == "Z"
+		 	encrypted << "A" 
+		  #instucting to rep. step forward in alphabet
+		  else letter = alphabet[letter].next! 
+		  	encrypted << letter
+		 end
+		 index += 1
 		end
-	end
-	str
+	encrypted
 end
 
 #output of encryption
-puts word(input)
+#puts encrypt(input)
+#puts decrypt(encrypt("swordfish"))
+def decrypt(str)
+	index = 0
+	# declare empty string we will add to
+	decrypted = ""
+	alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		# loop over the string's letters
+		while index < str.length
+		letter = str[index]
+		  if letter == ''
+		  	decrypted << ''
+		  #conditioning "z" as step back for "a"
+		  elsif letter == "a"
+		  	decrypted << "z"
+		  elsif letter == "A"
+		  	decrypted << "Z" 
+		#instucting to rep. step back in alphabet
+		  else back = alphabet.index(letter) - 1 
+		  	decrypted << alphabet[back]
+		 end
+		 index += 1
+		end
+	decrypted
+end
+
