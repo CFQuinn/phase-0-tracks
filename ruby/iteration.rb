b4dff2b1910febcee897fcaa144575e897177849
CFQuinn <chrisquinones55@gmail.com>
2016-11-07 09:04:19 -0600

    second part of 5.3

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -28,3 +28,65 @@ end
 sigil.each do |team, logo|
   puts "#{sigil} is the logo for #{house} in the NBA"
 end
+
+#Second Part of 5.3
+
+
+
+letters = ["a","b","c","d","e"]
+
+numbers ={
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4
+}
+
+p letters
+  letters.select!{|letter| letter>="c"}
+p letters
+
+p letters
+  letters.reject!{|letter| letter>"a"}
+p letters
+
+p numbers
+  numbers.select!{|number_spell,num| number_spell.length < num}
+p numbers
+
+p numbers
+  numbers.reject!{|number_spell,num| number_spell.length == num}
+p numbers
+
+
+
+
+letters = ["a","b","c","d","e"]
+
+numbers ={
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4
+}
+
+
+
+p letters
+  letters.delete_if{|letter| letter>"b"}
+p letters
+
+
+p numbers
+  numbers.delete_if{|number_spell,num| number_spell.length == num}
+p numbers
+
+puts "\n-----------------\n"
+
+p letters
+  letters.keep_if{|letter| letter>="d"}
+p letters
+
+p numbers
+  numbers.keep_if{|number_spell,num| number_spell.length < num}
+p numbers
