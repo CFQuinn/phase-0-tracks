24298f71363f4daf0cebb9975d5c058c367f5a51
CFQuinn <chrisquinones55@gmail.com>
2016-12-05 11:14:40 -0600

    crated game file and added program pseudocode

---
+++ ruby/guess_game/program.rb
@@ -0,0 +1,14 @@
+# A user types a word to be guessed 
+# Store word they type
+# Get the length of the word
+# Prints out empty underscores for the length of 
+# the word
+# Assigns length as the number of guesses
+
+# Until the number of guess is equal to the length of the word OR word is guessed
+# Takes a letter as a guess (input from user)
+# Guess count increases by one
+# IF the letter matches one from the word, print that letter in the line (replaces the blank dash) and
+# IF NOT, say "you will learn the ways one day young grasshopper, but today is not that day"
+
+# Once the guesses are up Or if word is guessed, print congratulatory message if win or taunting message if not
