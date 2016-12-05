b17a79e209022e462ad77829e4edc0dd589b46f9
CFQuinn <chrisquinones55@gmail.com>
2016-12-05 01:22:32 -0600

    added comments for puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+#SPECIES ------------------------
+#TBD
+#
+#CHARACTERISTICS ----------------
+#Tail Length: varies
+#Friendly: yes
+#Eye count: 2
+#Name: varies
+#Color: varies
+#fur type: varies
+#
+#BEHAVIOR -----------------------
+#Jump
+#chase tail 
+#roll over
