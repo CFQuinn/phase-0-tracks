8146fce63973316bb33da1b54806bd08202ac034
CFQuinn <chrisquinones55@gmail.com>
2016-12-05 02:29:15 -0600

    release1 of 6.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,26 +1,30 @@
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+	def initialize
+		puts "Initializing new puppy instance ..."
+	end
 
-  def speak(int)
-     int.times {puts "Woof!"} 
-   end
-   
-   def roll_over
-     puts "*rolls over*"
-   end
-   
-   def dog_years(human_years)
-     dog_years = human_years * 7
-     dog_years
-   end
+	def fetch(toy)
+	  puts "I brought back the #{toy}!"
+	  toy
+	end
+	
+	def speak(int)
+	   int.times {puts "Woof!"} 
+	end
+	
+	def roll_over
+	  puts "*rolls over*"
+	end
+	
+	def dog_years(human_years)
+	  dog_years = human_years * 7
+	  puts "#{dog_years}"
+	end
 
-   def dance(style)
-   	puts "Wow! I didn't know you could #{style}!"
-   end
+	def dance(style)
+		puts "Wow! I didn't know you could #{style}!"
+	end
 
 end
 
@@ -32,5 +36,5 @@ Ugly = Puppy.new
 Shadow.fetch("a beer")
 Don.speak(5)
 Kimbo.roll_over
-Ugly.dance(Salsa)
+Ugly.dance("Salsa")
 Shadow.dog_years(3)
