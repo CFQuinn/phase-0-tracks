6da92472549b0fea2b0c8711f930b08dba63d7b5
CFQuinn <chrisquinones55@gmail.com>
2016-12-05 04:17:36 -0600

    release 1 for 6.3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,9 @@
 class Santa
 
-	def initialize
+	def initialize(a_gender, an_ethnicity)
 		puts "Initializing Santa instance..."
+		@gender = a_gender
+		@ethnicity = an_ethnicity
 	end
 
 	def speak
@@ -12,10 +14,26 @@ class Santa
 		puts "That was a good #{cookie_type}!"
 	end
 
+	def reindeer_ranking
+		@reindeer = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	end
+
+	def age
+		@age = 0
+	end
+
 end
 
 Ben = Santa.new
 
-Ben.speak 
+#Ben.speak 
+#
+#Santa_bio = []
 
+gender_input = ["male", "female", "bi", "trans"]
+enthnicity_input = ["latino", "white", "black", "asian"]
+
+gender_input.length.times do |w|
+	Santa_bio << Santa.new(gender_input[w], enthnicity_input[w])
+end
 
