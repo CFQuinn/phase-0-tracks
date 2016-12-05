892ffd55f78bbd37b05618f62875f59cdc7c5156
CFQuinn <chrisquinones55@gmail.com>
2016-12-05 02:21:32 -0600

    release 0 of 6.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,32 @@ class Puppy
     toy
   end
 
+  def speak(int)
+     int.times {puts "Woof!"} 
+   end
+   
+   def roll_over
+     puts "*rolls over*"
+   end
+   
+   def dog_years(human_years)
+     dog_years = human_years * 7
+     dog_years
+   end
+
+   def dance(style)
+   	puts "Wow! I didn't know you could #{style}!"
+   end
+
 end
 
+Shadow = Puppy.new
+Don = Puppy.new
+Kimbo = Puppy.new
+Ugly = Puppy.new
+
+Shadow.fetch("a beer")
+Don.speak(5)
+Kimbo.roll_over
+Ugly.dance(Salsa)
+Shadow.dog_years(3)
